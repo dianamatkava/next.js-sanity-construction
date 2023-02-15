b21faff6359c9720f99a2abd167f01978efe8e70
Diana <dianamatkava@gmail.com>
2023-02-15 22:02:32 +0100

    L8

--- components-library/src/app-components/content/Content.tsx
+++ components-library/src/app-components/content/Content.tsx
@@ -2,5 +2,9 @@ import React from "react";
 
 
 export default function Content() {
-    return <h1>Content Works</h1>
+    return (
+        <div className="content-body-bg-dark h-main rounded-3xl">
+            <h1>Content Works</h1>
+        </div>
+    )
 }
--- components-library/src/app-components/main/Main.tsx
+++ components-library/src/app-components/main/Main.tsx
@@ -5,9 +5,9 @@ import Content from '../content/Content'
 export default function Main() {
     return (
         // TODO: navbar
-        <>
+        <div className='main-body-bg-dark min-h-screen'>
             <h1>Main works</h1>
             <Content />
-        </>
+        </div>
     )
 }

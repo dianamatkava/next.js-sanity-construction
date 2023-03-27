ac97fae99fa2fc2e5449c3000193fb1fb55fead9
DianaMatkava <dianamatkava@gmail.com>
2023-03-27 23:44:10 +0200

    Learn useEffect teory

--- components-library/src/components/header/base/Inline.tsx
+++ components-library/src/components/header/base/Inline.tsx
@@ -1,16 +1,24 @@
 import { InlineItem } from "../../../interfaces/HeaderInterfaces";
 import {IconComponent, IconKey} from "../../../defaults/iconMapper"
+import { useState } from "react";
 
 
 export default function Inline({...props}: InlineItem) {
     const InlineTag = props.tag as keyof JSX.IntrinsicElements;
-
-
+    const [color, setColor] = useState(props.color)
     return (
-        <a 
-            href="/#" 
-            className="flex items-center"> 
-            <span className="self-center text-xl font-semibold whitespace-nowrap mx-6">logoType</span>
-        </a>
+        props.link ? (
+            <InlineTag href={props.link} />
+        ) : (
+            <InlineTag href={props.link} />
+        )
+            
+        
+        // <a 
+        //     href="/#" 
+        //     className="flex items-center"> 
+        //     <span className="self-center text-xl font-semibold whitespace-nowrap mx-6">logoType</span>
+        // </a>
+    
     )
 }

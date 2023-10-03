bc0078953c6f9b783eee9e782ff1b6c49bc3fb5f
Diana <dianamatkava@gmail.com>
2023-10-03 16:47:15 +0200

    Adds Storybook

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './Button';
+import { Button } from '../components/ui/Button/Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -8,7 +8,7 @@ const meta = {
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'centered',
+    layout: 'centered', 
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
---
+++ src/utils/helpers.ts
@@ -0,0 +1,2 @@
+export {}
+// helper functions
